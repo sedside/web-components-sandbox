714f4923562f47b99c421ba96b9ab65127a2cdbe
sedside <thaess.aep@gmail.com>
2020-08-28 10:24:08 +0700

    ui kit demo

--- styleguide.config.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-
-module.exports = {
-    skipComponentsWithoutExample: true,
-    serverPort: 7070,
-    usageMode: 'expand',
-    ignore: ['**/style-files-to-copy.js']
-};
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,22 @@
 const path = require('path');
 const webpack = require('webpack'); // eslint-disable-line no-unused-vars
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
-
-const extractLessconfig = new MiniCssExtractPlugin({
-    filename: '[name].[hash:6].css',
-    chunkFilename: '[id].css'
-});
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
     template: 'index.html'
 });
 
-const definePluginConfig = new webpack.DefinePlugin({});
-
 const plugins = [
     HtmlWebpackPluginConfig,
-    extractLessconfig,
-    definePluginConfig,
-    new webpack.ProvidePlugin({
-        React: 'react'
-    })
 ];
 
-const assetsImgOptions = {
-    name: '[name].[hash:6].[ext]',
-    outputPath: 'assets/images'
-};
-
-const config = {
+module.exports = {
     mode: process.env.NODE_ENV || 'development',
     context: __dirname,
     entry: {
-        main: ['./src/entry.jsx']
+        main: ['./src/index.tsx']
     },
 
     output: {
@@ -43,12 +25,20 @@ const config = {
         filename: 'index.js'
     },
 
+    watch: process.env.NODE_ENV === 'development',
+
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: 300,
+        ignored: ['node_modules/**'],
+    },
+
     resolve: {
         modules: [
             path.resolve(__dirname, 'src'),
             path.resolve(__dirname, 'node_modules'),
         ],
-        extensions: ['.ts', '.tsx', '.js', '.jsx', '.less', '.scss', '.gif']
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
     },
 
     optimization: {
@@ -58,79 +48,9 @@ const config = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
                 test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: 'ts-loader'
-            },
-            {
-                enforce: 'pre',
-                test: /\.js$/,
-                loader: 'source-map-loader'
-            },
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.(less|css)$/,
-                exclude: /node_modules/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                        }
-                    },
-                    'postcss-loader',
-                    {
-                        loader: 'less-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.(scss|css)$/,
-                // exclude: /node_modules/,
-                use: [
-                    // MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader'
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.(jpg|png|gif|ico)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: assetsImgOptions
-                }
-            },
-            {
-                test: /\.svg$/,
-                use: [
-                    {
-                        loader: 'base64-inline-loader?limit=1000&name=[name].[ext]'
-                    }
-                ]
-            },
-            {
-                test: /\.(ttf|eot|woff|woff2)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'assets/fonts'
-                    }
-                }
+                include: /src/,
+                use: 'awesome-typescript-loader',
             }
         ]
     },
@@ -145,10 +65,4 @@ const config = {
         host: 'localhost',
         port: '5050'
     }
-};
-
-module.exports = function() {
-    require('./copy-styles-and-fonts');
-
-    return config;
 };
